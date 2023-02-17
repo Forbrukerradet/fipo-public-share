bee3b8da23b8cc4e1a493257a719144148eb8155
wims80 <wims80@gmail.com>
2023-02-17 09:36:39 +0100

    Script for preparing a linux util vm

---
+++ scripts/prepare_vm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## Update the system and install postgresql
+sudo apt-get update
+sudo apt-get install postgresql mysql mariadb
+
+## Download Microsoft's azcopy
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+
+## Copy azcopy to /usr/bin
+azcopy=$(ls -d azcopy*)
+sudo cp $azcopy/azcopy /usr/bin
