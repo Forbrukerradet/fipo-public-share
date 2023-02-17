844194087d03ceaf1b602cc7139689772d1f9220
wims80 <wims80@gmail.com>
2023-02-17 14:14:06 +0100

    Removed mariadb tools from the prepare_vm.sh script

--- scripts/prepare_vm.sh
+++ scripts/prepare_vm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-## Update the system and install postgresql
+## Update the system and install postgresql and mysql
 sudo apt-get update
-sudo apt-get install postgresql mysql mariadb
+sudo apt-get install postgresql mysql
 
 ## Download Microsoft's azcopy
 wget https://aka.ms/downloadazcopy-v10-linux
